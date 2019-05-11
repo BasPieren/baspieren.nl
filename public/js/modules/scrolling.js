dffad74f9796166c90b91f156e7fb9457079583c
bas.pieren@quicknet.nl <bas.pieren@quicknet.nl>
2019-05-11 16:06:10 +0200

    ✨ Added nav animation using scrollmagic.js

--- public/js/modules/scrolling.js
+++ public/js/modules/scrolling.js
@@ -1,4 +1,4 @@
-function  pageScrollingAni() {
+function  wipeNatural() {
   const controller = new ScrollMagic.Controller({
     		globalSceneOptions: {
     			triggerHook: 'onLeave',
@@ -12,9 +12,33 @@ function  pageScrollingAni() {
 					triggerElement: i
 				})
 				.setPin(i, {pushFollowers: false})
-				.addTo(controller);
+				.addTo(controller)
     })
 
 }
 
-export { pageScrollingAni }
+function scrollingClassToggle() {
+  const controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      duration: 1135
+    }
+  })
+
+	new ScrollMagic.Scene({triggerElement: "#bp-home-section"})
+					.setClassToggle(".bp-home-nav", "bp-active-page")
+					.addTo(controller)
+
+	new ScrollMagic.Scene({triggerElement: "#bp-about-section"})
+					.setClassToggle(".bp-about-nav", "bp-active-page")
+					.addTo(controller)
+
+	new ScrollMagic.Scene({triggerElement: "#bp-work-section"})
+					.setClassToggle(".bp-work-nav", "bp-active-page")
+					.addTo(controller)
+
+	new ScrollMagic.Scene({triggerElement: "#bp-contact-section"})
+					.setClassToggle(".bp-contact-nav", "bp-active-page")
+					.addTo(controller);
+}
+
+export { wipeNatural, scrollingClassToggle }
