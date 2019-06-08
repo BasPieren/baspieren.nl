f5ae717c73fd252ffed74887c743536301c583c1
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-08 20:11:32 +0200

    🔥 Removed unused code

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,3 @@
 'use strict';
 
-import * as typed from './modules/typing.js';
-import * as scroll from './modules/scrolling.js';
-
-typed.homeTypingAni()
-scroll.wipeNatural()
-scroll.scrollingClassToggle()
+console.log('Hello World!')
