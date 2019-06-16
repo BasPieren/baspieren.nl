e75a84b1de7c6750c0912464693cf742264e0a64
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-16 21:43:11 +0200

    ✨ Added typed-animation.js import

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
 import { headerBackgroundScroll } from './modules/header-background-scroll.js'
+import * as typed from './modules/typed-animation.js'
 
 headerBackgroundScroll()
+typed.basPierenHome()
