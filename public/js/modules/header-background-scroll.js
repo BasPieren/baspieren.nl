054c56ec93c69c0b00aa492144e23dedf0c1c89b
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-16 20:17:25 +0200

    ✨ Created headerBackgroundScroll()

---
+++ public/js/modules/header-background-scroll.js
@@ -0,0 +1,16 @@
+function headerBackgroundScroll() {
+    const scrollPos = 0,
+          header = document.querySelector('header')
+
+    window.addEventListener('scroll', () => {
+        let windowYOffset = window.scrollY
+
+        if (windowYOffset > scrollPos){
+            header.classList.add('header-background-color')
+        } else if (windowYOffset === scrollPos) {
+            header.classList.remove('header-background-color')
+        }
+    })
+}
+
+export { headerBackgroundScroll }
