0ee6275c44e3f9db5860a1344fa7bc139b905d2a
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-16 22:31:55 +0200

    ✨ Added mobile-nav.js import

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,9 @@
 'use strict';
 
+import { mobileNav } from './modules/mobile-nav.js'
 import { headerBackgroundScroll } from './modules/header-background-scroll.js'
 import * as typed from './modules/typed-animation.js'
 
+mobileNav()
 headerBackgroundScroll()
 typed.basPierenHome()
