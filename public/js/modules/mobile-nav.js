408d2fe90869c556e6a98651b09d20ba4b2e9ee4
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-17 20:56:09 +0200

    ✨ Added extra class .toggles

--- public/js/modules/mobile-nav.js
+++ public/js/modules/mobile-nav.js
@@ -1,10 +1,14 @@
 function mobileNav() {
     const body = document.querySelector('body'),
+          headerHeading = document.querySelector('header h4 span'),
           headerHamburger = document.querySelector('header button'),
           aside = document.querySelector('aside')
 
     headerHamburger.addEventListener('click', () => {
         body.classList.toggle('overflow-hidden')
+        headerHeading.classList.toggle('text-color-secondary')
+        headerHeading.classList.toggle('text-color-comment')
+        headerHamburger.classList.toggle('text-color-secondary')
         aside.classList.toggle('show')
     })
 }
