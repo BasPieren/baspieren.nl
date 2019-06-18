8d4f190fd8da63687f202553fdedd189d32e7e34
Bas Pieren <bas.pieren@quicknet.nl>
2019-06-18 22:00:29 +0200

    💬 Updated import urls

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
 "use strict"
 
-import { mobileNav } from "./modules/mobile-nav.js"
-import { headerBackgroundScroll } from "./modules/header-background-scroll.js"
+import { mobileNav } from "./modules/nav/mobile-nav.js"
+import { headerBackgroundScroll } from "./modules/header/header-background-scroll.js"
 import * as typed from "./modules/typed-animation.js"
 
 mobileNav()
